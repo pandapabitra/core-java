77d78193f725a50f83c6779c39a10afc30f9c7e9
Pabitra Panda <pk.pabitra18@gmail.com>
2021-04-08 08:45:33 +0530

    Control Statement, Looping and Assignment

--- src/com/mycompany/assignment1/Pattern2.java
+++ src/com/mycompany/assignment1/Pattern2.java
@@ -11,7 +11,7 @@ public class Pattern2 {
         int i,j;
         for(i = size; i>0; i--)
         {
-            for(j = 0;j<i;j++)
+            for(j = 0; j<i; j++)
             {
                 System.out.print("*");
             }
